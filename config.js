b821507f4acb59abdf387442d902ac354c7f461f
Xander Burke <xburke31@gmail.com>
2025-10-18 18:55:12 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-// config.js — Supabase configuration for ALPHAVERSE
+// config.js — Supabase config
 window.SUPABASE_CONFIG = {
   url: "https://qsojowjntexxxokrtkju.supabase.co",
   anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFzb2pvd2pudGV4eHhva3J0a2p1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA3NDgwMjQsImV4cCI6MjA3NjMyNDAyNH0.hjOdKypUPnsMtpkHjsNbuC_LMgKjioRY_wGNV6hNDnw"
